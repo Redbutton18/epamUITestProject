a5eb71dfa4bda93523cd73bfba391183756fef90
Redbutton18 <LenovoP780>
2021-04-12 17:08:22 +0300

    Selenoid with remote driver set up fix3

--- src/test/java/com/epam/BaseTest.java
+++ src/test/java/com/epam/BaseTest.java
@@ -2,6 +2,7 @@ package com.epam;
 
 import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -12,6 +13,7 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
@@ -21,7 +23,7 @@ import static com.epam.helpers.properties.UrlLinkProp.MAIN_URL;
 public class BaseTest {
     public String baseUrl = MAIN_URL;
     public String remoteURL = "http://10.0.2.15:4444/wd/hub";
-    public RemoteWebDriver driver;
+    public WebDriver driver;
 
 //    @BeforeTest
 //    public void setUp(){
@@ -31,28 +33,40 @@ public class BaseTest {
 //        Configuration.reportsFolder = "target/test-result/reports";
 //    }
 
-    @Parameters({"browser"})
     @BeforeMethod
-    public void setUp(String browserName){
-        DesiredCapabilities cap = new DesiredCapabilities();
-        cap.setCapability("enableVNC", true);
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("firefox");
+        capabilities.setVersion("86.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
 
-        if (browserName.equals("chrome")){
-            WebDriverManager.chromedriver().setup();
-            cap.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);
-        } else if (browserName.equals("firefox")){
-            WebDriverManager.firefoxdriver().setup();
-            cap.setCapability(CapabilityType.BROWSER_NAME, BrowserType.FIREFOX);
-        } else if (browserName.equals("opera")){
-            WebDriverManager.operadriver().setup();
-            cap.setCapability(CapabilityType.BROWSER_NAME, BrowserType.OPERA);
-        }
-        try{
-            driver = new RemoteWebDriver(new URL(remoteURL), cap);
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
-        } catch (MalformedURLException e){
-            e.printStackTrace();
-        }
+        driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(),
+        capabilities);
     }
+
+//    @Parameters({"browser"})
+//    @BeforeMethod
+//    public void setUp(String browserName){
+//        DesiredCapabilities cap = new DesiredCapabilities();
+//        cap.setCapability("enableVNC", true);
+//
+//        if (browserName.equals("chrome")){
+//            WebDriverManager.chromedriver().setup();
+//            cap.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);
+//        } else if (browserName.equals("firefox")){
+//            WebDriverManager.firefoxdriver().setup();
+//            cap.setCapability(CapabilityType.BROWSER_NAME, BrowserType.FIREFOX);
+//        } else if (browserName.equals("opera")){
+//            WebDriverManager.operadriver().setup();
+//            cap.setCapability(CapabilityType.BROWSER_NAME, BrowserType.OPERA);
+//        }
+//        try{
+//            driver = new RemoteWebDriver(new URL(remoteURL), cap);
+//            driver.manage().window().maximize();
+//            driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+//        } catch (MalformedURLException e){
+//            e.printStackTrace();
+//        }
+//    }
 }
